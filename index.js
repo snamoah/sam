06e6be2709955fd6c538ec705ad73c9114478d66
Samuel Amoah <snamoah@gmail.com>
2017-04-28 02:34:16 +0000

    change module name to sam

--- index.js
+++ index.js
@@ -1,90 +1,11 @@
 #!/usr/bin/env node --harmony
 
-var program = require('commander'),
-  $ = require('shelljs'),
-  path = require('path'),
-  co = require('co'),
-  prompt = require('co-prompt'),
-  ejs = require('ejs'),
-  fs = require('fs');
+var program = require('commander');
 
-var defaultModuleName = path.basename(process.cwd()),
-  moduleName = '',
-  author = '',
-  description = '',
-  modulePath = '';
-
-//program
-//  .command('create', { isDefault: true });
-
-function createModule() {
-  createDirs();
-  createConfigFiles();
-  createPackageJsonFile();
-};
-
-function createConfigFiles() {
-  copy('babelrc', '.babelrc');
-  copy('eslintrc', '.eslintrc');
-  copy('gitignore', '.gitignore');
-  copy('npmignore', '.npmignore');
-  copy('editorconfig', '.editorconfig');
-}
-
-function copy(source, destination) {
-  var sourcePath = path.join(__dirname, 'files/' + source); 
-  var destinationPath = path.join(modulePath, destination);
-  fs.writeFileSync(destinationPath, fs.readFileSync(sourcePath));
-}
-
-function template(file, data) {
-  var source = path.join(__dirname, file);
-  var sourceFile = fs.readFileSync(source, 'utf-8'); 
-  return ejs.render(sourceFile, data);
-}
+// commander prompt
+program
+  .command('g', 'generate boilerplate configuration files for es6, meteorjs, etc...')
+  .parse(process.argv);
 
 
-function createPackageJsonFile() {
-  var pkgJsonFile = template('files/_package.json', {
-    modulename: moduleName,
-    description: description,
-    author: author
-  });
-
-  var destinationPath = path.join(modulePath, 'package.json');
-  fs.writeFileSync(destinationPath, pkgJsonFile);
-}
-
-function createDirs() {
-  $.mkdir('-p', path.join(modulePath, 'lib'));
-  $.cp(path.join(__dirname, 'files/index.js'), path.join(modulePath, 'lib'));
-}
-
-function promptUser() {
-  co(function *() {
-    moduleName = yield prompt('name(' + defaultModuleName + '): ');
-    description = yield prompt('description: ');
-    author = yield prompt('author: ');
-    
-
-    moduleName = moduleName || defaultModuleName;
-    modulePath = path.basename(process.cwd())  == moduleName ? process.cwd() : path.join(process.cwd(), moduleName);
-
-    //create directory if user doesn't want to use current directory
-    if (process.cwd() !== modulePath) {
-      $.mkdir('-p', modulePath);
-      $.cd(modulePath);
-    }
-    
-    createModule();
-
-    //end when done
-    process.exit();
-  });
-
-};
-
-
-// Execution
-promptUser();
 
---
+++ sam-g.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node --harmony
+
+var program = require('commander'),
+  $ = require('shelljs'),
+  path = require('path'),
+  co = require('co'),
+  prompt = require('co-prompt'),
+  ejs = require('ejs'),
+  fs = require('fs');
+
+
+var defaultModuleName = path.basename(process.cwd()),
+  moduleName = '',
+  author = '',
+  description = '',
+  modulePath = '';
+
+
+function createModule() {
+  createDirs();
+  createConfigFiles();
+  createPackageJsonFile();
+};
+
+function createConfigFiles() {
+  generateBasicConfig();
+  copy('babelrc', '.babelrc');
+  copy('npmignore', '.npmignore');
+}
+
+function generateBasicConfig() {
+  copy('eslintrc', '.eslintrc');
+  copy('editorconfig', '.editorconfig');
+  copy('gitignore', '.gitignore');
+}
+
+function copy(source, destination) {
+  var sourcePath = path.join(__dirname, 'files/' + source); 
+  var destinationPath = path.join(modulePath, destination);
+  fs.writeFileSync(destinationPath, fs.readFileSync(sourcePath));
+}
+
+function template(file, data) {
+  var source = path.join(__dirname, file);
+  var sourceFile = fs.readFileSync(source, 'utf-8'); 
+  return ejs.render(sourceFile, data);
+}
+
+
+function createPackageJsonFile() {
+  var pkgJsonFile = template('files/_package.json', {
+    modulename: moduleName,
+    description: description,
+    author: author
+  });
+
+  var destinationPath = path.join(modulePath, 'package.json');
+  fs.writeFileSync(destinationPath, pkgJsonFile);
+}
+
+function createDirs() {
+  $.mkdir('-p', path.join(modulePath, 'lib'));
+  $.cp(path.join(__dirname, 'files/index.js'), path.join(modulePath, 'lib'));
+}
+
+function promptUser() {
+  co(function *() {
+    moduleName = yield prompt('name(' + defaultModuleName + '): ');
+    description = yield prompt('description: ');
+    author = yield prompt('author: ');
+
+
+    moduleName = moduleName || defaultModuleName;
+    modulePath = path.basename(process.cwd())  == moduleName ? process.cwd() : path.join(process.cwd(), moduleName);
+
+    //create directory if user doesn't want to use current directory
+    if (process.cwd() !== modulePath) {
+      $.mkdir('-p', modulePath);
+      $.cd(modulePath);
+    }
+
+    createModule();
+
+    //end when done
+    process.exit();
+  });
+
+};
+
+exports = Generator = {
+  es6: function es6() {
+    promptUser();      
+  },
+
+  basic: function basic() {
+    generateBasicConfig();
+    process.exit();
+  }
+};
+
+program
+  .arguments('<generator>', 'generator type [ es6, basic ]')
+  .action(function(type) {
+    Generator[type] && Generator[type]();
+  })
+  .parse(process.argv);
